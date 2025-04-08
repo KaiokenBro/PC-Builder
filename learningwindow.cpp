5dbacf6458089f5327613525f7949c5bd808f414
Harrison Doppelt <hdoppelt@gmail.com>
2025-04-07 18:54:06 -0600

    Added GPU, CPU, Ram, MB, Case.

--- learningwindow.cpp
+++ learningwindow.cpp
@@ -9,6 +9,36 @@ LearningWindow::LearningWindow(TestWindow* testWindow, QWidget* parent) :
     ui->setupUi(this);
     this->testWindow = testWindow;
 
+    // PC Case Image
+    QPixmap casePixmap(":/images/case.png");
+    ui->caseLabel->setPixmap(casePixmap);
+    ui->caseLabel->setScaledContents(true);
+
+    // Motherboard Image
+    QPixmap motherboardPixmap(":/images/motherboard.png");
+    ui->motherboardLabel->setPixmap(motherboardPixmap);
+    ui->motherboardLabel->setScaledContents(true);
+
+    // Graphics Card Image
+    QPixmap gpuPixmap(":/images/gpu.png");
+    ui->gpuLabel->setPixmap(gpuPixmap);
+    ui->gpuLabel->setScaledContents(true);
+
+    // CPU Image
+    QPixmap cpuPixmap(":/images/cpu.png");
+    ui->cpuLabel->setPixmap(cpuPixmap);
+    ui->cpuLabel->setScaledContents(true);
+
+    // Memory Image
+    QPixmap memoryPixmap(":/images/memory.png");
+    ui->memoryLabel->setPixmap(memoryPixmap);
+    ui->memoryLabel->setScaledContents(true);
+
+    // Ram Image
+    QPixmap ramPixmap(":/images/ram.png");
+    ui->ramLabel->setPixmap(ramPixmap);
+    ui->ramLabel->setScaledContents(true);
+
     // When testButton clicked, open testWindow
     connect(ui->testButton,
             &QPushButton::clicked,
